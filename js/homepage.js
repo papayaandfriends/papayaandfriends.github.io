0b42b091cebef1cbd5e945615a36d7ba3d253c71
papayaandfriends <65878255+papayaandfriends@users.noreply.github.com>
2020-05-24 20:56:50 -0500

    Add files via upload

---
+++ js/homepage.js
@@ -0,0 +1,10 @@
+$(function() {
+    //everything else you put into this file goes below here
+    $("#music").mouseover(function(
+
+
+    	$("img#music").hide();
+
+
+    //everything else you put into this file goes above here
+});
